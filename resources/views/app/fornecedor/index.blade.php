f3d2676d01ad01f704cfa0ec7bfcc7125303d76a
carlosgodspeed <carlosminibics@gmail.com>
2023-09-06 18:58:35 -0300

    .

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,1 +1,21 @@
 <h3>fornecedor ok</h3>
+
+@php
+/*
+   if() {
+
+   } elseif() {
+
+   } else {
+
+   }
+*/
+@endphp
+
+@if(count($fornecedores)> 0 && count($fornecedores) < 10)
+    <h3>os fornecedores estão online</h3>
+@elseif(count($fornecedores)> 10)
+    <h3>cadastrados os fornecedores</h3>
+@else
+    <h3>cadastra-se fornecedor</h3>
+@endif
