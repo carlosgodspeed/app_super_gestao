2be09e65d793bf3d5a7a3b13b6cacf8bc778f2bf
carlosgodspeed <carlosminibics@gmail.com>
2024-03-05 18:36:05 -0300

    migration criada e seeder criada

--- database/migrations/2024_03_05_203509_alter_fornecedores_softdelete.php
+++ database/migrations/2024_03_05_203509_alter_fornecedores_softdelete.php
@@ -4,26 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
-     * Rodar a migrations
+     * Run the migrations.
      */
     public function up(): void
     {
-        //
         Schema::table('fornecedores', function (Blueprint $table) {
-            $table->string('site', 150)->after('nome')->nullable();
+            $table->softDeletes();
         });
     }
 
     /**
-     * Reverter a migration.
+     * Reverse the migrations.
      */
     public function down(): void
     {
-        //
         Schema::table('fornecedores', function (Blueprint $table) {
-            $table->dropColumn('site');
+            //para remover colunas
+            // $table->dropColumn('uf');
+            // $table->dropColumn('email');
+            $table->dropSoftDeletes();
         });
     }
 };
